ce0bd96830df1cad7d8b72c7a3c93b5fc94da481
karthik-karotra <karthikpatel54@gmail.com>
2020-08-08 12:40:58 +0530

    ADDED : Code & Test To Handle Negative Scenarious For State Code CSV File

--- CensusAnalyser/CensorAnalyser.cs
+++ CensusAnalyser/CensorAnalyser.cs
@@ -33,7 +33,26 @@ namespace CensusAnalyser
 
         public string[] LoadStateCensusCSVData(string csvFilePath)
         {
+            if (!File.Exists(csvFilePath))
+            {
+                throw new CensusAnalyserException("File Not Found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+            if (Path.GetExtension(csvFilePath) != ".csv")
+            {
+                throw new CensusAnalyserException("Incorrect Type", CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE);
+            }
             censusData = File.ReadAllLines(csvFilePath);
+            if (censusData[0] != "SrNo,State Name,TIN,StateCode")
+            {
+                throw new CensusAnalyserException("Invalid Headers", CensusAnalyserException.ExceptionType.INVALID_HEADERS);
+            }
+            foreach (string data in censusData)
+            {
+                if (!data.Contains(","))
+                {
+                    throw new CensusAnalyserException("Invalid Delimiters In File", CensusAnalyserException.ExceptionType.INVALID_DELIMITER);
+                }
+            }
             return censusData.Skip(1).ToArray();
         }
     }
--- CensusAnalyserTest/CensusAnalyserTest.cs
+++ CensusAnalyserTest/CensusAnalyserTest.cs
@@ -11,6 +11,7 @@ namespace CensusAnalyserTest
         static string InvalidDeliminatorFilePath = @"D:\C-Sharp\CensusAnalyser\IncorrectDeliminatorCensusFile.csv";
         static string InvalidHeaderFilePath = @"D:\C-Sharp\CensusAnalyser\IncorrectHeaderCensusFile.csv";
         static string CSVStateCodeFilePath = @"D:\C-Sharp\CensusAnalyser\IndiaStateCode.csv";
+        static string InvalidDeliminatorStateCodeFilePath = @"D:\C-Sharp\CensusAnalyser\InvalidDeliminatorIndiaStateCode.csv";
         CensorAnalyser censusAnalyser;
 
         [SetUp]
@@ -83,6 +84,58 @@ namespace CensusAnalyserTest
         {
             string[] actual = censusAnalyser.LoadStateCensusCSVData(CSVStateCodeFilePath);
             Assert.AreEqual(37, actual.Length);
+        }
+
+        [Test]
+        public void GivenStateCodesCSVFile_WhenFileNotFound_ShouldThrowException()
+        {
+            try
+            {
+                censusAnalyser.LoadStateCensusCSVData(InvalidFilePath);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, ex.type);
+            }
+        }
+
+        [Test]
+        public void GivenStateCodesCSVFile_WhenIncorrectFileType_ShouldThrowException()
+        {
+            try
+            {
+                censusAnalyser.LoadStateCensusCSVData(InvalidCSVTypeFilePath);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE, ex.type);
+            }
+        }
+
+        [Test]
+        public void GivenStateCodesCSVFile_WhenIncorrectDeliminatorInFile_ShouldThrowException()
+        {
+            try
+            {
+                censusAnalyser.LoadStateCensusCSVData(InvalidDeliminatorStateCodeFilePath);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_DELIMITER, ex.type);
+            }
+        }
+
+        [Test]
+        public void GivenStateCodesCSVFile_WhenIncorrectHeadersInFile_ShouldThrowException()
+        {
+            try
+            {
+                censusAnalyser.LoadStateCensusCSVData(InvalidHeaderFilePath);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_HEADERS, ex.type);
+            }
         }
     }
 }
